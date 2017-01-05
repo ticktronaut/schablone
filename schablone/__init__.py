6df324e1b213e8e70c52164c6798f55b402de1d3
Martin <mh.myorbit@gmail.com>
2017-01-05 14:11:22 +0100

    Fix required packages and minor changes in setup.py

---
+++ schablone/__init__.py
@@ -0,0 +1,10 @@
+"""
+The schablone package exports the following modules and subpackages:
+
+    base - Basic SVG-file creation
+    generic - generic...
+    label - label specifics
+"""
+
+version_info = (0, 2, 'dev1')
+__version__ = '%d.%d.%s' % version_info
