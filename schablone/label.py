77972556dd6cbb06fd409318877bdb5e5697c062
nerdomat <ghandi_84@hotmail.com>
2017-01-21 12:37:16 +0100

    Changed design for smd_container. label_type and cut set must be set in constructor now. self.label_type and self.cut have been removed now.

--- schablone/label.py
+++ schablone/label.py
@@ -26,21 +26,19 @@ class smd_content_container(object):
         self.tolerance = ''
         self.temperature_coefficient = ''
         self.power = ''
-        self.tmpl_path = 'templates/label/smd_container/'
 
 
 class smd_container(generic):
-    def __init__(self):
-        log.debug("Instantiating class 'smd_content'.")
+    def __init__(self, label_type=None, cut=None):
+        
         super(smd_container, self).__init__()
 
-        #self.qr_content = "http://www.sappz.de"
-
+        self._fn_cut = '' 
         self.content = smd_content_container()
+        self.tmpl_path = 'templates/label/smd_container/' # todo: remove /
 
-        self.label_type = "mira_1"  #todo: link zu quelle #getter setter: remove all layers, reset layers
-        self.cut = False  #todo getter setter: remove all layers, reset layers
-        #self.__cut_list = []
+#        self.label_type = "mira_1"  #todo: link zu quelle #getter setter: remove all layers, reset layers
+#        self.cut = False  #todo getter setter: remove all layers, reset layers
 
         self.cpt_tspan = {
             'title': '',
@@ -51,40 +49,79 @@ class smd_container(generic):
             'power': ''
             # FixMe: Add voltage
         }
-        #		self.cpt_flowpara = {}
+        #self.cpt_flowpara = {}
         self.cpt_rect = {'matrix': ''}
-        # todo: self.cut_list
+
+        if label_type is None:
+            label_type = 'mira_1'
+        if cut is None:
+            cut = False
+
+        self.set_layers(label_type, cut) 
+
+        if label_type is 'mira_1' or 'mira_1a':  # type "1" and type "1a" seem to have label same size
+            self.width = '15mm'
+            self.height = '20mm'
+        elif label_type is 'mira_2':
+            raise RuntimeError('Label type mira_2 not supported, yet.')
+        elif label_type is 'mira_3':
+            raise RuntimeError('Label type mira_3 not supported, yet.')
+        elif label_type is "mira_4":
+            raise RuntimeError('Label type mira_4 not supported, yet.')
+        else:
+            raise RuntimeError('Unknown type of label: ' + self.label_type)
+
+
+    def set_layers(self, label_type, cut=None):
+
+        if cut is None:
+            cut=True
+
+        self.layer.clear('smd_container')
+        self.layer.clear('smd_container_cut')
+
+        self.layer.add(
+            pkg_resources.resource_filename(
+                'schablone', self.tmpl_path + label_type
+                + '/font.svg'), group='smd_container')
+
+        self.layer.add(
+            pkg_resources.resource_filename(
+                'schablone', self.tmpl_path + label_type
+                + '/matrix.svg'), group='smd_container')
+
+        if not cut:
+            self.layer.add(
+                pkg_resources.resource_filename(
+                    'schablone', self.tmpl_path + label_type + '/frame.svg'), group='smd_container')
+        else:
+            self.layer.add(
+                pkg_resources.resource_filename(
+                    'schablone', self.tmpl_path + label_type + '/frame_cut.svg'), group='smd_container_cut')
+
 
     # hier wird die einzige Moeglichkeit der Basisklasse 
     # die Hoehe und Breite zu setzen ueberschrieben
     # Achtung ist überschreibend
     def save(self, fn=None):
 
-        # set width and height (reconfigure everytime time save is called)
-        self.width = '15mm'
-        if self.label_type == 'mira_1' or 'mira_1a':  # type "1" and type "1a" seem to have label same size
-            self.width = '15mm'
-            self.height = '20mm'
-        elif self.label_type == 'mira_2':
-            raise RuntimeError('Label type mira_2 not supported, yet.')
-        elif self.label_type == 'mira_3':
-            raise RuntimeError('Label type mira_3 not supported, yet.')
-        elif self.label_type == "mira_4":
-            raise RuntimeError('Label type mira_4 not supported, yet.')
-        else:
-            raise RuntimeError('Unknown type of label: ' + self.label_type)
-        log.debug("Label Type is " + self.label_type + " with size w=" + self.width + ", h=" + self.height)
+        if fn is not None:
+            self._fn = fn
 
-        # self.fn is set after this point 
+        self.cpt_tspan['title'] = self.content.title
+        self.cpt_tspan['value'] = self.content.value
+        self.cpt_tspan['package'] = self.content.package
+        self.cpt_tspan['tolerance'] = self.content.tolerance
+        self.cpt_tspan[
+            'temperature_coefficient'] = self.content.temperature_coefficient
+        self.cpt_tspan['power'] = self.content.power
+
+        # self._fn is set after this point 
         # todo: think about better solution for self._fn (explicit is better than implicit) 
         super(smd_container, self).save_frame(fn)
 
-#        # save data matrix code with unique id (using bash)
-#        fn, fext = os.path.splitext(self._fn)
-#        fn_qr = fn + '_qr' + '.png'
-#        os.system(
-#            'uuidgen | tr [[:upper:]] [[:lower:]] | tr -d \'-\'  | sed -e \'s/\(.\{12\}\).*/\\1/\' | dmtxwrite -s 16x16 -o '
-#            + fn_qr)
+        self._fn_cut = super(smd_container, self)._fn_sub_str(self._fn,
+                                                              '_cut')
 
         # save data matrix code with unique id (python3 solution)
         fn, fext = os.path.splitext(self._fn)
@@ -99,44 +136,9 @@ class smd_container(generic):
         encoder.save(fn_qr)
         
         self.cpt_rect['matrix'] = fn_qr
-        
 
-        # todo: rethink the following ...
-        # two ways to organize addition of layers
-        # delete all layers and reset them 
-        # - in save function (any time file is saved, as done here)
-        # - in setter function for self.cut (any time self.cut is changed)
-        self.layer.clear()
-        self.layer.add(
-            pkg_resources.resource_filename(
-                'schablone', 'templates/label/smd_container/' + self.label_type
-                + '/font.svg'))
-
-        if not self.cut:
-            self.layer.add(
-                pkg_resources.resource_filename(
-                    'schablone', 'templates/label/smd_container/' +
-                    self.label_type + '/frame.svg'))
-        else:
-            #			self.layer.default_lr = 'cut'
-            self._fn_cut = super(smd_container, self)._fn_sub_str(self._fn,
-                                                                  "_cut")
-            self.layer.add(
-                pkg_resources.resource_filename(
-                    'schablone', 'templates/label/smd_container/' +
-                    self.label_type + '/frame_cut.svg'), 0.0, 0.0, 1.0, 'cut')
-            super(smd_container, self).save_layers(self._fn_cut, self._fn,
-                                                   'cut')
-
-        super(smd_container, self).save_layers()
-
-        self.cpt_tspan['title'] = self.content.title
-        self.cpt_tspan['value'] = self.content.value
-        self.cpt_tspan['package'] = self.content.package
-        self.cpt_tspan['tolerance'] = self.content.tolerance
-        self.cpt_tspan[
-            'temperature_coefficient'] = self.content.temperature_coefficient
-        self.cpt_tspan['power'] = self.content.power
+        super(smd_container, self).save_layers(self._fn_cut, self._fn, 'smd_container_cut')
+        super(smd_container, self).save_layers(group='smd_container')
         super(smd_container, self).save_substitutes()
 
         #from cairosvg.surface import PDFSurface
@@ -149,8 +151,6 @@ class smd_container(generic):
                 raise RuntimeError('List of svg_list is empty')
             else:
                 svg_list = self._fn_list
-
-#todo: raise error
 
         if fn == None:
             fn = "default_Ax.svg"
