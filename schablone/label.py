deefad5d14b285bf33081ae5e3351448c67c6950
nerdomat <ghandi_84@hotmail.com>
2017-01-30 16:01:51 +0100

    self.label_type is label_type.

--- schablone/label.py
+++ schablone/label.py
@@ -28,9 +28,11 @@ class smd_content_container(object): #FixMe cap_content_container | res_content_
 
 class smd_container(generic):
 
-    def __init__(self, label_type='mira_1', tmpl_path=None, size=None):
-        log.debug("Instantiating class 'smd_container'.")
+    def __init__(self, label_type='mira_1', tmpl_path=None, size=None, cut=False):
+        
         super(smd_container, self).__init__()
+ 
+        self._fn_cut = ''
         self.content = smd_content_container()
 
         if tmpl_path is None: # set default template path
@@ -39,13 +41,10 @@ class smd_container(generic):
             self.tmpl_path = tmpl_path
             self.content._is_custom_template = True
 
-        log.debug("tmpl_path: " + str(self.tmpl_path))
+#        self.label_type = label_type  #todo: link zu quelle #getter setter: remove all layers, reset layers
+#        self.cut = False  #todo getter setter: remove all layers, reset layers
 
-        self.label_types = ('mira_1', 'licefa_n1') # tuple with valid label types (create getter?)
-        self.label_type = label_type  #todo: link zu quelle #getter setter: remove all layers, reset layers
-        log.debug("Label type from init(): " + str(label_type))
-        log.debug("Label type: " + self.label_type)
-        self.cut = False  #todo getter setter: remove all layers, reset layers
+        self.label_types = ('mira_1', 'licefa_n1') # tuple with valid label types (create getter?) 
 
         if size is not None:
             if len(size) != 2:
@@ -62,53 +61,111 @@ class smd_container(generic):
             'power': ''
             # FixMe: Add voltage
         }
+        #self.cpt_flowpara = {}
         self.cpt_rect = {'matrix': ''}
-        # todo: self.cut_list
 
-    # hier wird die einzige Moeglichkeit der Basisklasse 
-    # die Hoehe und Breite zu setzen ueberschrieben
-    # Achtung ist überschreibend
-    def save(self, fn=None):
-        # Currently all other parameters are set in __init__()
+        # layers are set here
+        self._set_layers(label_type, cut)
 
-        # set width and height (reconfigure everytime time save is called)
-        log.info("Check for valid label_type")
-        if self.label_type == 'mira_1' or self.label_type == 'mira_1a':  # type "1" and type "1a" seem to have the same label size
+        if label_type == 'mira_1' or label_type == 'mira_1a':  # type "1" and type "1a" seem to have the same label size
             self.width = '15mm'
             self.height = '20mm'
-        elif self.label_type == 'mira_2':
+        elif label_type == 'mira_2':
             raise RuntimeError('Label type mira_2 not supported, yet.')
-        elif self.label_type == 'mira_3':
+        elif label_type == 'mira_3':
             raise RuntimeError('Label type mira_3 not supported, yet.')
-        elif self.label_type == "mira_4":
+        elif label_type == "mira_4":
             raise RuntimeError('Label type mira_4 not supported, yet.')
-        elif self.label_type == 'licefa_n1': # SMD-Box N1
+        elif label_type == 'licefa_n1': # SMD-Box N1
             raise RuntimeError('Label type licefa_n1 not supported, yet.')
             self.width = '22mm'
             self.height = '29mm'
-        elif self.label_type == 'licefa_n2': # SMD-Box N2
+        elif label_type == 'licefa_n2': # SMD-Box N2
             raise RuntimeError('Label type licefa_n2 not supported, yet.')
             self.width = '29mm'
             self.height = '42mm'
-        elif self.label_type == 'licefa_n3': # SMD-Box N3
+        elif label_type == 'licefa_n3': # SMD-Box N3
             raise RuntimeError('Label type licefa_n2 not supported, yet.')
             self.width = '42mm'
             self.height = '56mm'
         else:
             # No standard label type found, so must be custom type.
             # Check if a custom template path is specified, otherwise raise error.
-            log.debug("Attempt to use custom label type '" + self.label_type + "'")
+            log.debug("Attempt to use custom label type '" + label_type + "'")
             if not self.content._is_custom_template:
                 log.error("Please specify template path to custom label type.")
                 raise RuntimeError("Please specify template path to custom label type.")
             # FixMe: use size from parameter
             self.width = '15mm' # self._custom_width
             self.height = '20mm' # self._custom_height
-        log.debug("Label type is '" + self.label_type + "' with size w=" + self.width + ", h=" + self.height)
+
+
+
+    def _set_layers(self, label_type, cut=None):
+
+        if cut is None:
+            cut=True
+
+        self.layer.clear('smd_container_font')
+        self.layer.clear('smd_container_matrix')
+        self.layer.clear('smd_container_frame')
+        self.layer.clear('smd_container_cut')
+
+#        self.layer.add(
+#            pkg_resources.resource_filename(
+#                'schablone', self.tmpl_path + label_type
+#                + '/font.svg'), group='smd_container_font')
+#
+#        self.layer.add(
+#            pkg_resources.resource_filename(
+#                'schablone', self.tmpl_path + label_type
+#                + '/matrix.svg'), group='smd_container_matrix')
+#
+#        if not cut:
+#            self.layer.add(
+#                pkg_resources.resource_filename(
+#                    'schablone', self.tmpl_path + label_type + '/frame.svg'), group='smd_container_frame')
+#        else:
+#            self.layer.add(
+#                pkg_resources.resource_filename(
+#                    'schablone', self.tmpl_path + label_type + '/frame_cut.svg'), group='smd_container_cut')
+
+        path = self.tmpl_path + '/' + label_type
+
+        self.layer.add(path + '/font.svg', group='smd_container_font')
+        self.layer.add(path + '/matrix.svg', group='smd_container_matrix')
+        if not cut:
+            self.layer.add(path + '/frame.svg', group='smd_container_frame')
+        else:
+            #self._fn_cut = super(smd_container, self)._fn_sub_str(self._fn, "_cut")
+            self.layer.add(path + '/frame_cut.svg', group='smd_container_cut')
+            #super(smd_container, self).save_layers(self._fn_cut, self._fn, 'cut')
+
+
+
+    # hier wird die einzige Moeglichkeit der Basisklasse 
+    # die Hoehe und Breite zu setzen ueberschrieben
+    # Achtung ist überschreibend
+    def save(self, fn=None):
+        # Currently all other parameters are set in __init__()
+ 
+        if fn is not None:
+            self._fn = fn
+
+        # todo check if ... in self.cpt_tspan.keys():
+        self.cpt_tspan['title'] = self.content.title
+        self.cpt_tspan['value'] = self.content.value
+        self.cpt_tspan['package'] = self.content.package
+        self.cpt_tspan['tolerance'] = self.content.tolerance
+        self.cpt_tspan['temperature_coefficient'] = self.content.temperature_coefficient
+        self.cpt_tspan['power'] = self.content.power
 
         # self.fn is set after this point 
         # todo: think about better solution for self._fn (explicit is better than implicit) 
         super(smd_container, self).save_frame(fn)
+
+        self._fn_cut = super(smd_container, self)._fn_sub_str(self._fn,
+                                                              '_cut')
 
         # save data matrix code with unique id (python3 solution)
         fn, fext = os.path.splitext(self._fn)
@@ -122,53 +179,22 @@ class smd_container(generic):
         encoder = DataMatrixEncoder(uuid_str)
         encoder.save(fn_qr)
 
-        self.cpt_rect['matrix'] = fn_qr
+        if 'matrix' in self.cpt_rect.keys():
+            self.cpt_rect['matrix'] = fn_qr
 
-        # todo: rethink the following ...
-        # two ways to organize addition of layers
-        # delete all layers and reset them 
-        # - in save function (any time file is saved, as done here)
-        # - in setter function for self.cut (any time self.cut is changed)
-        self.layer.clear()
+        # save layers
+        for group in self.layer.tmpl_lr.keys():
+            if group == 'smd_container_cut':
+                super(smd_container, self).save_layers(self._fn_cut, self._fn, 'smd_container_cut')
+            else:
+                super(smd_container, self).save_layers(group=group)
 
-        path = self.tmpl_path + '/' + self.label_type
-
-        self.layer.add(path + '/font.svg')
-        if not self.cut:
-            self.layer.add(path + '/frame.svg')
-        else:
-            self._fn_cut = super(smd_container, self)._fn_sub_str(self._fn, "_cut")
-            self.layer.add(path + '/frame_cut.svg', 0.0, 0.0, 1.0, 'cut')
-            super(smd_container, self).save_layers(self._fn_cut, self._fn, 'cut')
-
-#        if self.content._is_custom_template:
-#            log.info("Attempt to use template path from user...")
-#            self.layer.add(path+ '/font.svg') # better use path add func?
-#            if not self.cut:
-#                self.layer.add(path + '/frame.svg')
-#            else:
-#                self._fn_cut = super(smd_container, self)._fn_sub_str(self._fn, "_cut")
-#                self.layer.add(path + '/frame_cut.svg', 0.0, 0.0, 1.0, 'cut')
-#                super(smd_container, self).save_layers(self._fn_cut, self._fn, 'cut')
-#        else:
-#            log.info("Use templates from package resource...")
-#            self.layer.add(pkg_resources.resource_filename('schablone', path + '/font.svg'))
-#            if not self.cut:
-#                self.layer.add(pkg_resources.resource_filename('schablone', path + '/frame.svg'))
-#            else:
-#                self._fn_cut = super(smd_container, self)._fn_sub_str(self._fn, "_cut")
-#                self.layer.add(pkg_resources.resource_filename('schablone', path + '/frame_cut.svg'), 0.0, 0.0, 1.0, 'cut')
-#                super(smd_container, self).save_layers(self._fn_cut, self._fn, 'cut')
-
-        super(smd_container, self).save_layers()
-
-        self.cpt_tspan['title'] = self.content.title
-        self.cpt_tspan['value'] = self.content.value
-        self.cpt_tspan['package'] = self.content.package
-        self.cpt_tspan['tolerance'] = self.content.tolerance
-        self.cpt_tspan['temperature_coefficient'] = self.content.temperature_coefficient
-        self.cpt_tspan['power'] = self.content.power
+        # save substitutes
         super(smd_container, self).save_substitutes()
+
+        # FixMe: park following code in script or documentation
+        #from cairosvg.surface import PDFSurface
+        #PDFSurface.convert(src, write_to=open('output.pdf', 'w'))
 
     def saveAx(self, fn=None, ax='a4', svg_list=None):
         if svg_list is None:
@@ -177,8 +203,6 @@ class smd_container(generic):
                 raise RuntimeError('List of svg_list is empty')
             else:
                 svg_list = self._fn_list
-
-#todo: raise error
 
         if fn == None:
             fn = "default_Ax.svg"
